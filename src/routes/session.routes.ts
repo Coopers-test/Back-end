1454b1157794738741f31e03c647d49ca26dcde0
Natália Favaro Cavicchioli <na.cavicchioli@gmail.com>
2023-02-02 16:15:05 -0300

    feat: login implemented

--- src/routes/session.routes.ts
+++ src/routes/session.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import UserLoginController from "../controllers/users/userLogin.controller";
 
 const routes = Router();
 
 export const sessionsRoutes = () => {
+  routes.post("/login", UserLoginController);
+
   return routes;
 };
