bdb2951985209299ace7ed86b99f2c86e26610ef
Natália Favaro Cavicchioli <na.cavicchioli@gmail.com>
2023-02-03 14:24:57 -0300

    feat: deploy configs

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,26 +1,49 @@
-import { DataSource } from "typeorm";
+import "reflect-metadata";
 import "dotenv/config";
+import path from "path";
+import { DataSource, DataSourceOptions } from "typeorm";
 
-const AppDataSource = new DataSource(
-  process.env.NODE_ENV === "test"
-    ? {
-        type: "sqlite",
-        database: ":memory:",
-        synchronize: true,
-        entities: ["src/entities/*.ts"],
-      }
-    : {
-        type: "postgres",
-        host: process.env.DB_HOST,
-        port: 5432,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB,
-        logging: true,
-        synchronize: false,
-        entities: ["src/entities/*.ts"],
-        migrations: ["src/migrations/*.ts"],
-      }
-);
+const setDataSourceConfig = (): DataSourceOptions => {
+  const entitiesPath: string = path.join(__dirname, "./entities/**.{js,ts}");
+  const migrationsPath: string = path.join(
+    __dirname,
+    "./migrations/**.{js,ts}"
+  );
 
-export default AppDataSource;
+  const nodeEnv = process.env.NODE_ENV;
+
+  if (nodeEnv === "production") {
+    return {
+      type: "postgres",
+      url: process.env.DB_URL,
+      entities: [entitiesPath],
+      migrations: [migrationsPath],
+    };
+  }
+
+  if (nodeEnv === "test") {
+    return {
+      type: "sqlite",
+      database: ":memory:",
+      synchronize: true,
+      entities: [entitiesPath],
+    };
+  }
+
+  return {
+    type: "postgres",
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: 5432,
+    database: process.env.DB,
+    synchronize: false,
+    logging: true,
+    entities: [entitiesPath],
+    migrations: [migrationsPath],
+  };
+};
+
+const dataSourceConfig = setDataSourceConfig();
+
+export default new DataSource(dataSourceConfig);
